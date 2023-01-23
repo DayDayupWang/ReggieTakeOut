7b108af77fc87627db24b4b6b56249dc90dfe3ac
Dirtygold <wangyh4031@163.com>
2023-01-23 20:57:57 +0800

    Redis环境

---
+++ src/main/java/com/example/reggie/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.example.reggie.config;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的key序列化器为    JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer((new StringRedisSerializer()));
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+}
